31ec47c2ceb43a326dfefc58710063687f341057
jv <ljw@live.jp>
2020-03-08 23:23:00 +0800

    feat: templates

---
+++ lib/generators/react-component/templates/.umirc.ts
@@ -0,0 +1,19 @@
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: false,
+        dynamicImport: false,
+        dll: false,
+        routes: {
+          exclude: [],
+        },
+      },
+    ],
+  ],
+};
