6d8391432aadde1af847eca6fd336220a9c7ffc7
jv <ljw@live.jp>
2020-03-19 22:56:05 +0800

    refactor: rollup

--- lib/generators/react-component/templates/.fatherrc.ts
+++ lib/generators/react-component/templates/.fatherrc.ts
@@ -1,4 +1,19 @@
 export default {
-  umd: true,
-  esm: true,
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+      },
+    ],
+  ],
+  cssModules: {
+    generateScopedName: '[name]-[local]',
+  },
+  injectCSS: true,
+  lessInBabelMode: true,
+  esm: 'rollup',
+  cjs: 'rollup',
 };
